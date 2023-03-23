a8f33f44450572e6201b311fb6ae1f7a71802ec8
Ashok Kuvaraja <60499432+ashok-kuvaraja@users.noreply.github.com>
2023-03-23 18:49:12 +0530

    Sample has been added.

---
+++ MauiProject/MauiProject/CustomLabel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiProject
+{
+    public class CustomLabel : Label
+    {
+        public bool IsLabelMeasured;
+
+        public CustomLabel()
+        {
+            this.IsLabelMeasured = false;
+        }
+
+        protected override Size MeasureOverride(double widthConstraint, double heightConstraint)
+        {
+            this.IsLabelMeasured = true;
+            return base.MeasureOverride(widthConstraint, heightConstraint);
+        }
+    }
+}
---
+++ MauiProject/MauiProject/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiProject;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+        label.Loaded += Label_Loaded;
+	}
+
+    private void Label_Loaded(object sender, EventArgs e)
+    {
+        if (label.IsLabelMeasured)
+        {
+            label.Text = "Label is measured when invoking its loaded event";
+        }
+        else
+        {
+            label.Text = "Label isn't measured when invoking its loaded event";
+        }
+    }
+}
+
